ceb5478ab15d795ae6cd972fe4171f19a7c958b6
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-07-23 17:57:10 -0300

    refactor: resolve regular expresion match

--- src/api/utils.py
+++ src/api/utils.py
@@ -10,19 +10,19 @@ def iol_decoder_hook(dct):
     for k, v in dct.items():
         if isinstance(v, str):
             # 2021-11-24T17:00:22.082Z
-            if re.match("(\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{3}Z)", v):
+            if re.match(r"(\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{3}Z)", v):
                 dct[k] = datetime.utcfromtimestamp(
                     pd.to_datetime(v, format="%Y-%m-%dT%H:%M:%S.%fZ").timestamp()
                 )
 
             # 2021-11-24T17:00:22.082
-            elif re.match("(\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{1,3})", v):
+            elif re.match(r"(\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{1,3})", v):
                 dct[k] = datetime.utcfromtimestamp(
                     pd.to_datetime(v, format="%Y-%m-%dT%H:%M:%S.%f").timestamp()
                 )
             # 2021-11-24T16:04:25.2370547-03:00
             elif re.match(
-                "(\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{7}[+-]\d{2}\:\d{2})", v
+                r"(\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{7}[+-]\d{2}\:\d{2})", v
             ):
                 dct[k] = datetime.utcfromtimestamp(
                     pd.to_datetime(v, format="%Y-%m-%dT%H:%M:%S.%f%z").timestamp()
@@ -30,14 +30,15 @@ def iol_decoder_hook(dct):
 
             # Fri, 26 Nov 2021 18:15:14 GMT
             elif re.match(
-                "([a-zA-Z]{3}, \d{2} [a-zA-Z]{3} \d{4} \d{2}\:\d{2}\:\d{2} [A-Z]{3})", v
+                r"([a-zA-Z]{3}, \d{2} [a-zA-Z]{3} \d{4} \d{2}\:\d{2}\:\d{2} [A-Z]{3})",
+                v,
             ):
                 dct[k] = datetime.utcfromtimestamp(
                     pd.to_datetime(v, format="%a, %d %b %Y %H:%M:%S %Z").timestamp()
                 )
 
             # 0001-01-01T00:00:00
-            elif re.match("(\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2})", v):
+            elif re.match(r"(\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2})", v):
                 dct[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S")
 
     return dct
